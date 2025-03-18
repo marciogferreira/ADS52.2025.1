338e59ae574092363e01c8f67b36baf98b45052c
Márcio Ferreira <hmarciogferreira@gmail.com>
2025-03-17 21:11:06 -0300

    AJustes

---
+++ my-app/app/ajuda.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Ajuda() {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text>Página de Ajuda</Text>
+        </View>
+    )
+}
